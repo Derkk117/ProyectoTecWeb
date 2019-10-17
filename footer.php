b04f6024fd4a7ef4d5875f185b51b536bce020c2
derkk117 <derkk117@gmail.com>
2019-10-17 11:21:57 -0500

    De regreso a HTML

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <ul>
-        <li><p>&copy; Todos los derechos reservados</p></li>
-        <li><a href="#"><img src="./Imagenes/in.jpg" alt="Instragram"></a></li>
-        <li><a href="#"><img src="./Imagenes/fa.png" alt="Facebook"></a></li>
-        <li><a href="#"><img src="./Imagenes/tww.png" alt="Twitter"></a></li>
-    </ul>
-</footer>
